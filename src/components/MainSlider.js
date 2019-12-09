3533484e81dcf0e218d7f1505361c7c402dccd94
Eugene Revutskiy <revytskiy.evgen@gmail.com>
2019-12-09 21:58:38 +0200

    4 commit

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -6,12 +6,71 @@ import anime from 'animejs/lib/anime.es.js';
 import ParicilesSkills from './ParicilesSkills';
 import Skills from './Skills';
 import ProjectItem from './ProjectItem';
+import project_1 from '../img/project_1.PNG';
+import project_2 from '../img/project_2.PNG';
+import project_3 from '../img/project_3.PNG';
+import project_4 from '../img/project_4.PNG';
+import project_5 from '../img/project_5.PNG';
+import project_6 from '../img/project_6.PNG';
+import project_7 from '../img/project_7.PNG';
+import project_8 from '../img/project_8.PNG';
 
 
 export class MainSlider extends Component {
-    constructor(props) {
+    constructor(props){
         super(props)
-        this.state = { projects: [] }
+        this.state={
+            items: [
+                {
+                    path: project_1,
+                    linkProject: "https://eugenere95.github.io/weight_converter/",
+                    linkGithub: "https://github.com/EugeneRe95/weight_converter",
+                    name:"Weight converter"
+                },
+                {
+                    path: project_2,
+                    linkProject: "https://eugenere95.github.io/rps_game/",
+                    linkGithub: "https://github.com/EugeneRe95/rps_game",
+                    name:"Rock Paper Scissors Game"
+                },
+                {
+                    path: project_3,
+                    linkProject: "https://eugenere95.github.io/fullscreen_slider/",
+                    linkGithub: "https://github.com/EugeneRe95/fullscreen_slider",
+                    name:"Fullscreen Slider"
+                },
+                {
+                    path: project_4,
+                    linkProject: "https://eugenere95.github.io/fancy_form/",
+                    linkGithub: "https://github.com/EugeneRe95/fancy_form",
+                    name:"Fancy Form"
+                },
+                {
+                    path: project_5,
+                    linkProject: "https://eugenere95.github.io/dynamic_lp/",
+                    linkGithub: "https://github.com/EugeneRe95/dynamic_lp",
+                    name:"Dynamic Landing Page"
+                },
+                {
+                    path: project_6,
+                    linkProject: "https://eugenere95.github.io/bookmarker/",
+                    linkGithub: "https://github.com/EugeneRe95/bookmarker",
+                    name:"Bookmarker"
+                },
+                {
+                    path: project_7,
+                    linkProject: "https://eugenere95.github.io/todo_list-ciklum_task_internship/",
+                    linkGithub: "https://github.com/EugeneRe95/todo_list-ciklum_task_internship",
+                    name:"Todo list"
+                },
+                {
+                    path: project_8,
+                    linkProject: "https://eugenere95.github.io/book_list/",
+                    linkGithub: "https://github.com/EugeneRe95/book_list",
+                    name:"Book list"
+                }
+            ]
+        }
     }
     componentDidMount() {
         var textWrapper = document.querySelectorAll('.heading p');
@@ -27,9 +86,9 @@ export class MainSlider extends Component {
                 delay: (el, i) => 80 * i
             });
 
-        fetch('./portfolio_items.json')
-            .then(response => response.json())
-            .then(items => this.setState({ projects: items }))
+        // fetch('../portfolio_items.json')
+        //     .then(response => response.json())
+        //     .then(items => this.setState({ projects: items }))
     }
     render() {
         return (
@@ -39,7 +98,7 @@ export class MainSlider extends Component {
                 <div id="portfolio">
                     <div className="portfolio-container">
                         {
-                            this.state.projects.map((item, index) => {
+                            this.state.items.map((item, index) => {
                                 return <ProjectItem path={item.path} key={index} linkProject={item.linkProject} linkGithub={item.linkGithub} name={item.name} animDelay={index * 300} />
                             })
                         }</div>
